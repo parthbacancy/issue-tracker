ac4390300d175f882f93fe3b18a5c8384b0a765d
parthbacancy <parth.patel1@bacancy.com>
2024-07-18 17:06:21 +0530

    Update Issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,7 +40,11 @@ const IssueForm = ({ issue }: Props) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) {
+        await axios.patch("/api/issues/" + issue.id, data);
+      } else {
+        await axios.post("/api/issues", data);
+      }
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
@@ -74,7 +78,7 @@ const IssueForm = ({ issue }: Props) => {
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
         <Button disabled={isSubmitting}>
-          Submit new Issue
+          {issue ? "Update Issue" : "Submit new Issue"}{" "}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
